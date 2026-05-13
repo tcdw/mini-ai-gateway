c2cb548f19c498a010a5a7c5ba561b0056d8e5fd
tcdw <tcdw2011@gmail.com>
2026-05-14 00:01:09 +0800

    refactor: migrate config from JSON to TOML

--- scripts/sync-models.ts
+++ scripts/sync-models.ts
@@ -1,4 +1,4 @@
-import config from "../config.json" with { type: "json" };
+import config from "../config.toml";
 
 // ---------------------------------------------------------------------------
 // Types
